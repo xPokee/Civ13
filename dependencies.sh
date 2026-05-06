0f4cd92d6adffc0401691830ac89149bd356ebfe
Alexis <catmc8565@gmail.com>
2026-05-06 07:50:24 -0400

    TGS

---
+++ dependencies.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#Project dependencies file
+#Final authority on what's required to fully build the project
+
+# byond version
+export BYOND_MAJOR=516
+export BYOND_MINOR=1681
+
+#rust_g git tag
+export RUST_G_VERSION=4.2.0
+
+#node version
+export NODE_VERSION_LTS=18.20.8
+
+# SpacemanDMM git tag
+export SPACEMAN_DMM_VERSION=suite-1.11
+
+# Python version for mapmerge and other tools
+export PYTHON_VERSION=3.9.0
+
+# Auxmos repository name
+export AUXMOS_REPOSITORY=Putnam3145/auxmos
+
+# Auxmos git tag
+export AUXMOS_VERSION=v2.3.0
+
+# Extools git tag
+export EXTOOLS_VERSION=v0.0.7
